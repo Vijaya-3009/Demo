2018d23aa0040ea12fbeaba289639ee4b004a8fb
Vijaya-3009 <133640689+Vijaya-3009@users.noreply.github.com>
2024-01-08 12:16:45 +0530

    myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title='Demon Slayer')
 st.header("Main Lead")
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Tanjiro Kamado")
   st.image("./Tanjiro.jpg", caption="Tanjiro Kamado", width=300,use_column_width=True)
@@ -10,5 +10,9 @@ with col2:
   st.subheader("Nezuko Kamado")
   st.image("./nezuko.jpg", caption="Nejuko Kamado", width=300,use_column_width=True)
   st.write("Nezuko Kamado is a Demon")
+with col3:
+  st.subheader("Tanjiro fight")
+  st.image("./Tanjiro.gif", caption="Tanjiro fight", width=300,use_column_width=True)
+  st.write("Tanjiro's fight with blood moon")
         
           
